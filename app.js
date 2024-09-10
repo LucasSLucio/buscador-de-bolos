bb0e7dd45c84f0b4d48a9b3bc7c16693c6ae3563
Lucas Silva <lucasestudos64@gmail.com>
2024-09-10 11:15:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa = document.getElementById
+    ("campo-pesquisa").value
+
+   // se campoPesquisa for uma string sem nada 
+   if(!campoPesquisa ) {
+    section.innerHTML = "<p>favor inserir alguma busca no campo</p>" 
+    return
+   }
+   campoPesquisa = campoPesquisa.toLowerCase()
+  
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags ="";
+    // Itera sobre cada dado e cria o HTML correspondente
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) ||tags.includes(campoPesquisa) ) {
+    //  // Cria um novo elemento HTML para cada resultado  
+        resultados += `
+        <div class="item-resultado"> 
+        <h2>
+            <a href="#" target="_blank">${dado.titulo}</a> </h2>
+          <p class="descricao-meta">${dado.descricao}</p> 
+          <a href=${dado.link} target="_blank">Receita completa</a> </div>
+      `;
+    }
+    }
+    
+    if (!resultados) {
+         resultados = "<p>Nada foi encontrado</p>" 
+
+    }
+    // Atualiza o conteúdo da seção com os resultados gerados
+    section.innerHTML = resultados;
+  }
+ 
+
+
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "bolo de cenoura",
+        descricao :"Para um bolo de cenoura delicioso e úmido, você vai precisar de: cenouras raladas, ovos, óleo, açúcar, farinha de trigo e fermento em pó. O preparo é bem simples: bata os ingredientes líquidos no liquidificador, misture aos ingredientes secos e leve para assar. Para um toque especial, adicione especiarias como canela ou noz-moscada. E para finalizar, uma cobertura cremosa de chocolate ou uma calda de açúcar caramelizada deixam o bolo ainda mais irresistível.",
+        link : "https://www.tudogostoso.com.br/receita/2532-bolo-de-cenoura-com-cobertura-de-chocolate.html", 
+        tags: "noura liquidificador liqui lik liki likidificador"
+    },
+    {
+      titulo : "bolo de chocolate", 
+      descricao : "Para um bolo de chocolate irresistível, você vai precisar de: chocolate em pó, ovos, açúcar, óleo, farinha de trigo, fermento em pó e leite. O preparo é simples e rápido: misture os ingredientes secos, adicione os ingredientes úmidos e leve para assar. Para uma cobertura deliciosa, use ganache de chocolate ou brigadeiro.",
+      link : "https://www.tudogostoso.com.br/receita/62547-a-melhor-receita-de-bolo-de-chocolate.html",
+      tags: "brigadeiroo brigaa pó late"
+    },
+    {
+        titulo: "bolo prestigio",
+        descricao : "O clássico bolo prestígio é feito com uma massa úmida de chocolate e um recheio cremoso de coco ralado e leite condensado. Para prepará-lo, basta seguir a receita tradicional: prepare a massa, asse o bolo, prepare o recheio e monte o bolo. Uma delícia para qualquer ocasião!",
+        link : "https://www.tudogostoso.com.br/receita/51484-bolo-de-prestigio.html",
+        tags: "pestigio gio prestigi"
+    }
+    ];
